95d147705119ece7f4dabcf3b153dd548eae7f30
Oscar Viteri <oscar.viteri7@hotmail.com>
2024-09-29 15:12:10 +0200

    add array and arroz (push and pop) #54

---
+++ staff/oscar-viteri/playground/array/Array.prototype.push.js
@@ -0,0 +1,18 @@
+console.log('TEST Array.prototype.push')
+
+console.log('CASE add 400 to nums')
+
+var nums = [100, 200, 300]
+var length = nums.push(400)
+// [100, 200, 300, 400] (4)
+console.log(length)
+//4
+
+console.log('CASE and F to chars')
+
+var chars = ['A', 'B', 'C', 'D', 'E']
+var length = chars.push('F')
+console.log(chars)
+// ['A', 'B', 'C', 'D', 'E', 'F'] (6)
+console.log(length)
+// 6
