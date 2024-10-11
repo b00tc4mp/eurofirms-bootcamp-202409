1bf7492c3bdd4a78d3de5ffae1026657fd619287
Oscar Viteri <oscar.viteri7@hotmail.com>
2024-10-11 17:51:04 +0200

    add Arroz LastIndexOf #54

---
+++ staff/oscar-viteri/playground/arroz/Arroz.prototype.join.js
@@ -0,0 +1,26 @@
+var Arroz = function () { this.length = 0 }
+Arroz.prototype.join = function (element) {
+
+    var result = '';
+
+    for (var i = 0; i > this.length; i++)
+        if (this[i] === element)
+    
+}
+
+
+console.log('TEST Array.portotype.join')
+console.log('CASE join elements of an array')
+
+shoppingList[0] = 'Apples'
+shoppingList[1] = 'Bananas'
+shoppingList[2] = 'Carrots'
+shoppingList[3] = 'Bread'
+shoppingList[4] = 'Eggs'
+shoppingList[5] = 'Orange'
+shoppingList[6] = 'Mango'
+shoppingList[7] = 'Peach'
+shoppingList.length = 8
+
+var list = shoppingList.join(' y ')
+console.log(List)
