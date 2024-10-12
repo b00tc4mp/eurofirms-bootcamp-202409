adb2021352647b69125f9a6250f579815aefbae7
Laura Garrido <lauragarrido76@hotmail.com>
2024-10-12 14:03:58 +0200

    add array at method,includes method and indexOf method test #51

--- staff/laura-garrido/playground/array/Array.prototype.includes.js
+++ staff/laura-garrido/playground/array/Array.prototype.includes.js
@@ -13,3 +13,39 @@ var friends = ['Pedro', 'Silvia', 'Laura', 'Maria']
 var includesFriends = friends.includes('Pepe')
 console.log(includesFriends)
 // false
+
+console.log('CASE check cars includes ferrari')
+var cars = ['lambo', 'cinquecento', 'fiesta', 'm5']
+var contains = cars.includes('ferrari')
+console.log(contains)
+// false
+
+console.log('CASE check cars includes fiesta')
+
+var cars = ['lambo', 'cinquecento', 'fiesta', 'm5']
+var contains = cars.includes('fiesta')
+console.log(contains)
+// true
+
+console.log('CASE check cars includes lambo from index 1')
+
+var cars = ['lambo', 'cinquecento', 'fiesta', 'm5']
+var contains = cars.includes('lambo', 1)
+console.log(contains)
+// false
+
+// TODO add negative index (offset) test case
+
+console.log('CASE check cars includes lambo from offset -1')
+
+var cars = ['lambo', 'cinquecento', 'fiesta', 'm5']
+var contains = cars.includes('lambo', -1)
+console.log(contains)
+// false
+
+console.log('CASE check cars includes cinquecento from offset -3')
+
+var cars = ['lambo', 'cinquecento', 'fiesta', 'm5']
+var contains = cars.includes('cinquecento', -3)
+console.log(contains)
+// true
--- staff/laura-garrido/playground/array/Array.prototype.indexOf.js
+++ staff/laura-garrido/playground/array/Array.prototype.indexOf.js
@@ -1,15 +1,57 @@
 console.log('TEST array.prototype.indexOf')
 
-console.log('CASE return the index of "tomato" on plants')
+console.log('CASE get index of tomato')
 
 var plants = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato']
 var indexOfPlants = plants.indexOf('tomato')
 console.log(indexOfPlants)
 // 4
 
-console.log('CASE return the index of "Pepe" on friends')
+console.log('CASE get index of Pepe')
 
 var friends = ['Pedro', 'Silvia', 'Laura', 'Maria']
 var indexOfFriends = friends.indexOf('Pepe')
 console.log(indexOfFriends)
 // -1
+
+console.log('CASE get index of bison')
+
+var beasts = ['ant', 'bison', 'camel', 'duck', 'bison']
+var index = beasts.indexOf('bison')
+console.log(index)
+// 1
+
+console.log('CASE get index of bison starting at index 2')
+
+var beasts = ['ant', 'bison', 'camel', 'duck', 'bison']
+var index = beasts.indexOf('bison', 2)
+console.log(index)
+// 4
+
+console.log('CASE get index of giraffe')
+
+var beasts = ['ant', 'bison', 'camel', 'duck', 'bison']
+var index = beasts.indexOf('giraffe')
+console.log(index)
+// -1
+
+console.log('CASE get index of giraffe starting at offset -4')
+
+var beasts = ['ant', 'bison', 'camel', 'duck', 'bison']
+var index = beasts.indexOf('giraffe', -4)
+console.log(index)
+// -1
+
+console.log('CASE get index of duck starting at offset -4')
+
+var beasts = ['ant', 'bison', 'camel', 'duck', 'bison']
+var index = beasts.indexOf('duck', -4)
+console.log(index)
+// 3
+
+console.log('CASE get index of duck starting at offset -3')
+
+var beasts = ['ant', 'bison', 'camel', 'duck', 'bison']
+var index = beasts.indexOf('duck', -3)
+console.log(index)
+// 4
