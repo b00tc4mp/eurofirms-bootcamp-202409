d6bdef9e35d0ed370a7855bd26d4849804158efd
Laura Garrido <lauragarrido76@hotmail.com>
2024-10-12 15:42:22 +0200

    add arroz at method,includes method and some method test and implementation  #51

--- staff/laura-garrido/playground/arroz/Arroz.prototype.at.1.js
+++ staff/laura-garrido/playground/arroz/Arroz.prototype.at.1.js
@@ -1,40 +1,10 @@
 var Arroz = function () { this.length = 0 }
 
 Arroz.prototype.at = function (index) {
-    if (index > -1)
-        return this[index]
-    else
-        return this[this.length + index]
+    return this[index]
 }
 
-console.log('TEST arroz.prototype.at')
-
-console.log('CASE get element at offset -1')
-
-var plants = new Arroz()
-plants[0] = "broccoli"
-plants[1] = "cauliflower"
-plants[2] = "cabbage"
-plants[3] = "kale"
-plants[4] = "tomato"
-plants.length = 5
-
-var atPlants = plants.at(-1)
-console.log(atPlants)
-// "tomato"
-
-console.log('CASE get element at offset -1')
-
-var friends = new Arroz()
-friends[0] = "Pedro"
-friends[1] = "Silvia"
-friends[2] = "Laura"
-friends[3] = "Maria"
-friends.length = 4
-
-var atFriends = friends.at(-1)
-console.log(atFriends)
-// "Maria"
+console.log('TEST Arroz.prototype.at')
 
 console.log('CASE get element at index 2')
 
--- staff/laura-garrido/playground/arroz/Arroz.prototype.includes.3.js
+++ staff/laura-garrido/playground/arroz/Arroz.prototype.includes.3.js
@@ -1,7 +1,12 @@
 var Arroz = function () { this.length = 0 }
 
 Arroz.prototype.includes = function (searchElement, fromIndex) {
-    for (var i = (fromIndex === undefined ? 0 : fromIndex); i < this.length; i++) {
+    var i
+
+    if (fromIndex === undefined) i = 0
+    else i = fromIndex
+
+    for (; i < this.length; i++) {
         var element = this[i]
 
         if (element === searchElement) return true
@@ -10,34 +15,7 @@ Arroz.prototype.includes = function (searchElement, fromIndex) {
     return false
 }
 
-console.log('TEST arroz.prototype.includes')
-
-console.log('CASE check plants includes tomato')
-
-var plants = new Arroz()
-plants[0] = "broccoli"
-plants[1] = "cauliflower"
-plants[2] = "cabbage"
-plants[3] = "kale"
-plants[4] = "tomato"
-plants.length = 5
-
-var includesPlants = plants.includes('tomato')
-console.log(includesPlants)
-// true
-
-console.log('CASE check friends includes Pepe')
-
-var friends = new Arroz()
-friends[0] = "Pedro"
-friends[1] = "Silvia"
-friends[2] = "Laura"
-friends[3] = "Maria"
-friends.length = 4
-
-var includesFriends = friends.includes('Pepe')
-console.log(includesFriends)
-// false
+console.log('TEST Arroz.prototype.includes')
 
 console.log('CASE check cars includes ferrari')
 
@@ -100,34 +78,5 @@ cars[4] = 'fiesta'
 cars[5] = 'lambo'
 cars.length = 6
 var contains = cars.includes('lambo', 1)
-console.log(contains)
-// true
-
-// TODO add negative index (offset) test case
-
-console.log('CASE check cars includes lambo from offset -1')
-
-var cars = new Arroz
-cars[0] = 'lambo'
-cars[1] = 'cinquecento'
-cars[2] = 'fiesta'
-cars[3] = 'm5'
-cars[4] = 'fiesta'
-cars.length = 5
-var contains = cars.includes('lambo', -1)
-console.log(contains)
-// true
-
-console.log('CASE check cars includes lambo from offset -1 (2)')
-
-var cars = new Arroz
-cars[0] = 'lambo'
-cars[1] = 'cinquecento'
-cars[2] = 'fiesta'
-cars[3] = 'm5'
-cars[4] = 'fiesta'
-cars[5] = 'lambo'
-cars.length = 6
-var contains = cars.includes('lambo', -1)
 console.log(contains)
 // true
