d45041cf840a6d0bd80ea432df6b46644bcff564
Oscar Viteri <oscar.viteri7@hotmail.com>
2024-10-17 22:05:44 +0200

    modified file concat #54

--- staff/oscar-viteri/playground/arroz/Arroz.prototype.concat.js
+++ staff/oscar-viteri/playground/arroz/Arroz.prototype.concat.js
@@ -1,37 +1,55 @@
+var Arroz = function () { this.length = 0 }
+// une dos array y devuelve un nuevo array
+// crear un nuevo arroz que contendra todos los arroz
+// meter los elementos del primer arroz y nuestro nuevo arroz
+// meter los elementos del segundo arroz en el nuevo arroz
+// longitud controlada de ese nuevo arroz
+Arroz.prototype.concat = function (elementsToConcat) {
+    var newArrozList = new Arroz
+    for (var i = 0; i < this.length; i++) {
+        newArrozList[i] = this[i]
+        newArrozList.length++
+        /* 
+        {0 = "meat",
+        1 = "fish",
+        2 = "cookies",
+        length = 3}
+         */
+    }
+    for (var i = 0; i < elementsToConcat.length; i++) {
+        newArrozList[newArrozList.length] = elementsToConcat[i]
+        newArrozList.length++
+        /*
+        {0 = "meat",
+        1 = "fish",
+        2 = "cookies",
+        3 = "beer",
+        4 = "doritos",
+        5 = "cakes",
+        length = 6}
+         */
+    }
+    return newArrozList
+}
+
 console.log('TEST Array.prototype.concat')
 console.log('CASE it is used to join two or more arrays')
 
-var Arroz = function () { this.length = 0 }
-
-Arroz.prototype.concat = function (elementToConcat) {
-    var result = new Arroz
-    for (var i = 0; i < this.length; i++) {
-        result[i] = this[i]
-        result.length
-    }
-    for (var i = 0; i < elementToConcat.length; i++) {
-        result[result.length] = elementToConcat[i]
-        result.length++
-    }
-    return result
-}
-var shoppinList = new Arroz
+var shoppingList = new Arroz
 
 shoppingList[0] = 'beer'
 shoppingList[1] = 'doritos'
-shoppingList.length = 2
+shoppingList[2] = 'cakes'
+shoppingList.length = 3
 
 var foodList = new Arroz
 
 foodList[0] = 'meat'
 foodList[1] = 'fish'
-foodList.length = 2
+foodList[2] = 'cookies'
+foodList.length = 3
 
-var fullShoppingList = new Arroz
-
-fullShoppingList = shoppinList.concat(foodList)
-
-console.log('shoppingList->', shoppinList)
-
-console.log('foodList->', foodList)
+var fullShoppingList = foodList.concat(shoppingList);
 console.log('fullshoppingList->', fullShoppingList)
+
+// [ 'beer', 'doritos', 'cakes', 'meat', 'fish', 'cookies' ]
