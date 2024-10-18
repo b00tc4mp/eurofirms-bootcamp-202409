6a64460d8db8770fce233fe5734ba079c203d57f
Mariops89 <mariops89@hotmail.com>
2024-10-18 22:26:52 +0200

    add example made by alumns to revise #52

--- staff/mario-perez/playground/array/Array.prototype.some.js
+++ staff/mario-perez/playground/array/Array.prototype.some.js
@@ -1,7 +1,7 @@
 /*
 El método some() comprueba si al menos un elemento del array cumple con la condición implementada 
 por la función proporcionada.
-*/
+
 
 console.log('TEST Array.prototype.some')
 
@@ -29,4 +29,59 @@ var callBackFunction = function (element) {
 }
 
 console.log(arr.some(callBackFunction))
+// false*/
+
+
+
+
+console.log('TEST Array.prototype.some')
+
+console.log('CASE find if exist fruits with the letter M ')
+
+var fruits = ['Banana', 'Mango', 'Piña', 'Fresa', 'Manzana']
+
+var exists1 = fruits.some(function (fruit) {
+    if (fruit.includes('M') || fruit.includes('m'))
+        return true
+    // return (fruit.includes('M') || fruit.includes('m'))
+})
+
+console.log(exists1)
+// true
+
+
+console.log('CASE find phone smartphone')
+
+var phones = ['Blackberry', 'Motorola', 'Nokia']
+
+var smartphone = phones[3]
+
+var exists2 = phones.some(function (phone) {
+    if (phones.includes(smartphone)) {
+        return true
+    } else {
+        return false
+    }
+})
+console.log(exists2)
 // false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
