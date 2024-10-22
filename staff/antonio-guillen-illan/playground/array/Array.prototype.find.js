c9a006537c56dd72a9ae26360b17559d4757df42
Antonio Guillen Illan <antonio181994.agi@gmail.com>
2024-10-22 20:08:11 +0200

    new file Array.prototype.find 87#

--- staff/antonio-guillen-illan/playground/array/Array.prototype.find.js
+++ staff/antonio-guillen-illan/playground/array/Array.prototype.find.js
@@ -3,12 +3,36 @@ console.log('TEST Array.prototype.find')
 console.log('CASE find first name with length greater that 5')
 
 var names = [
-    'Peter'
-    'John'
-    'Max'
-    'James'
-    'Constantin'
-    'Mario'
-    'Tea'
-    'Luis'
+    'Peter',
+    'John',
+    'Max',
+    'James',
+    'Constantin',
+    'Mario',
+    'Tea',
+    'Luis',
 ]
+
+var name = names.find(function (name) {
+    return name.length > 5
+});
+console.log(name)
+// Constantin
+
+console.log('CASE find first name with character x')
+
+/* var names = [
+    'Peter',
+    'John',
+    'Max',
+    'James',
+    'Constantin',
+    'Mario',
+    'Tea',
+    'Luis',
+]
+*/
+varname = names.find(function (name) {
+    return name.includes('x')
+});
+console.log(name)
