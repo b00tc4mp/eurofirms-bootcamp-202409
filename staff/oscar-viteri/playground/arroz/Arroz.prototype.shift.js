aea9379f6700b08c78dbbf46e8aa4a837bff523f
Oscar Viteri <oscar.viteri7@hotmail.com>
2024-10-23 22:15:05 +0200

    add Arroz shift and slice #54

--- staff/oscar-viteri/playground/arroz/Arroz.prototype.shift.js
+++ staff/oscar-viteri/playground/arroz/Arroz.prototype.shift.js
@@ -1,18 +1,19 @@
 var Arroz = function () { this.length = 0 }
 
 Arroz.prototype.shift = function () {
-    //Quita un elemento de un array
-    //crear un array
-    var i
-    //recorrer el objeto
-    for (var i = 0; i < this.lenght; i++) {
-        //quita el elemento
-        delete this[this.length - 1]
-        //devolver el resultado en otro array
-        this.length--
-        return -1
+    //crear una variable con el valor de la primera posicion
+    var days = this[0]
+
+    for (var i = 0; i < this.length - 1; i++) {
+
+        //asignamos la posicion de i la siguiente
+        this[i] = this[i + 1] // this[i++]
     }
+    delete this[this.length - 1]
+    this.length--
+    return days
 }
+
 console.log('TEST Arroz.prototype.shift')
 
 var daysOfTheWeek = new Arroz
@@ -20,10 +21,11 @@ daysOfTheWeek[0] = 'Monday'
 daysOfTheWeek[1] = 'Tuesday'
 daysOfTheWeek[2] = 'Wednesday'
 daysOfTheWeek[3] = 'Thursday'
-daysOfTheWeek[4] = ' Friday'
+daysOfTheWeek[4] = 'Friday'
 daysOfTheWeek[5] = 'Saturday'
 daysOfTheWeek[6] = 'Sunday'
 daysOfTheWeek.length = 7
 var holiday = daysOfTheWeek.shift()
-console.log(holiday)
-console.log(daysOfTheWeek)
+console.log('holiday -> ', holiday)
+console.log('daysOfTheWeek -> ', daysOfTheWeek)
+// [ 'Tuesday', 'Wednesday', Thursday', 'Friday, 'Saturday', 'Sunday' ]
