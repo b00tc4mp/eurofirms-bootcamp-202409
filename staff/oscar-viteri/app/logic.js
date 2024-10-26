c050aae85d578d110c896f378427a6e8f2967342
Oscar Viteri <oscar.viteri7@hotmail.com>
2024-10-26 17:54:30 +0200

    implement id in users, use localStorage for users #98

--- staff/oscar-viteri/app/logic.js
+++ staff/oscar-viteri/app/logic.js
@@ -1,9 +1,11 @@
-function authenticateUser(username, password) {
+function loginUser(username, password) {
     if (typeof username !== 'string') throw new Error('invalid username')
     if (username.length < 4) throw new Error('invalid username length')
 
     if (typeof password !== 'string') throw new Error('invalid password')
     if (password.length < 8) throw new Error('invalid password length')
+
+    var users = JSON.parce(localStorage.user)
 
     var user = users.find(function (user) {
         return user.username === username && user.password === password
@@ -11,7 +13,7 @@ function authenticateUser(username, password) {
 
     if (user === undefined) throw new Error('wrong credentials')
 
-    return user
+    sessionStorage.userId = user.id
 }
 
 function registerUser(name, email, username, password) {
@@ -25,11 +27,14 @@ function registerUser(name, email, username, password) {
     var indexOfAt = email.indexOf('@')
     var indexOfDot = email.indexOf('.')
     if (indexOfDot < indexOfAt) throw new Error('invalid email format')
+
     if (typeof username !== 'string') throw new Error('invalid username')
     if (username.length < 4) throw new Error('invalid username length')
 
     if (typeof password !== 'string') throw new Error('invalid password')
     if (password.length < 8) throw new Error('invalid password length')
+
+    var users = JSON.parce(localStorage.users)
 
     var user = users.find(function (user) {
         return user.email === email || user.username === username
@@ -38,10 +43,35 @@ function registerUser(name, email, username, password) {
     if (user !== undefined) throw new Error('user already exists')
 
     user = {}
+    user.id = uuid()
     user.name = name
     user.email = email
     user.username = username
     user.password = password
 
     users.push(user)
+
+    localStorage.users = JSON.stringify(users)
+}
+
+function getUserName() {
+    var users = JSON.parse(localStorage.users)
+
+    var user = users.find(function (user) {
+        return user.id === sessionStorage.userId
+    })
+
+    if (user === undefined) throw new Error('user not found')
+
+    return user.name
+}
+
+function isUserLoggedIn() {
+    if (sessionStorage.userId !== undefined) return true
+
+    return false
+}
+
+function logoutUser() {
+    delete sessionStorage.userId
 }
