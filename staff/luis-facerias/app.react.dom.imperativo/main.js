5f3d5f27eb44d606b60eab2612b9b7b8b0364448
Luis Facerías <luis.facerias@gmail.com>
2024-10-27 21:28:30 +0100

    update app.react.dom.imperativo #132

--- staff/luis-facerias/app.react.dom.imperativo/main.js
+++ staff/luis-facerias/app.react.dom.imperativo/main.js
@@ -1,35 +1,21 @@
+// Esta prueba de react DOM, no va a avanzar más en la codificación.
+// Sólo es una muestra de esta opción
+
 var title = React.createElement('h1', null, 'App')
 
 var welcomeTitle = React.createElement('h2', null, 'Welcome!')
 
 var welcomeIntroPart1 = React.createElement(React.Fragment, null, 'Please, ')
-
-
-var welcomeIntro = React.createElement('p', null, welcomeIntroPart1)
+var welcomeRegisterLink = React.createElement('a', { href: ''}, 'Register')
+var welcomeIntroPart3 = React.createElement(React.Fragment, null, ' or ')
+var welcomeLoginLink = React.createElement('a', { href: '' }, 'Login')
+var welcomeIntroPart5 = React.createElement(React.Fragment, null, '.')
+//
+var welcomeIntro = React.createElement('p', null, [welcomeIntroPart1, welcomeRegisterLink, welcomeIntroPart3, welcomeLoginLink, welcomeIntroPart5])
 
 var welcomeView = React.createElement('main', null, [welcomeTitle, welcomeIntro])
-
 
 var rootElement = document.querySelector('#root')
 var root = ReactDOM.createRoot(rootElement)
 
 root.render([title, welcomeView])
-
-
-// Estructura de WELCOME VIEW
-
-/*
-html
-    head
-    body
-        main
-            h2
-                text
-            p
-                text
-                a
-                text
-                a
-                text
-*/
-
