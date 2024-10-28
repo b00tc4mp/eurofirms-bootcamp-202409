09acd52c45ed4e5deb707c9c8f60564e37907832
Thea272 <kintiraia@yahoo.es>
2024-10-28 22:17:29 +0100

    add new versions of App, App.8 & App.9 to #106

--- staff/tea-kintiraia/app/data.js
+++ staff/tea-kintiraia/app/data.js
@@ -1,10 +1,9 @@
-//data
+//mover los users to localStorage
 
 var users = []
 
-users[0] = { name: 'Ji Rafa', email: 'ji@rafa.com', username: 'jirafa', password: '123123123' }
-users[1] = { name: 'Ele Fante', email: 'ele@fante.com', username: 'elefante', password: '123123123' }
-users[2] = { name: 'Coco Drilo', email: 'coco@drilo.com', username: 'cocodrilo', password: '123123123' }
+users[0] = { id: uuid(), name: 'Ji Rafa', email: 'ji@rafa.com', username: 'jirafa', password: '123123123' }
+users[1] = { id: uuid(), name: 'Ele Fante', email: 'ele@fante.com', username: 'elefante', password: '123123123' }
+users[2] = { id: uuid(), name: 'Coco Drilo', email: 'coco@drilo.com', username: 'cocodrilo', password: '123123123' }
 
-
-
+localStorage.users = JSON.stringify(users)
--- staff/tea-kintiraia/app/logic.js
+++ staff/tea-kintiraia/app/logic.js
@@ -5,13 +5,15 @@ function authenticateUser(username, password) {
     if (typeof password !== 'string') throw new Error('invalid password')
     if (password.length < 8) throw new Error('invalid password length')
 
+    var users = JSON.parse(localStorage.users)
+
     var user = users.find(function (user) {
         return user.username === username && user.password === password
     })
 
     if (user === undefined) throw new Error('wrong crendecials')
 
-    return user
+    sessionStorage.userId = user.id
 }
 
 function registerUser(name, email, username, password) {
@@ -19,7 +21,7 @@ function registerUser(name, email, username, password) {
     if (name.length < 1) throw new Error('invalid name length')
 
     if (typeof email !== 'string') throw new Error('invalid email')
-    if (email.length < 6) throw new Error('invalid email')
+    if (email.length < 6) throw new Error('invalid email length')
     if (!email.includes('@')) throw new Error('invalid email format')
     if (!email.includes('.')) throw new Error('invalid email format')
     var indexOfAt = email.indexOf('@')
@@ -33,6 +35,8 @@ function registerUser(name, email, username, password) {
     if (typeof password !== 'string') throw new Error('invalid password')
     if (password.length < 8) throw new Error('invalid password length')
 
+    var users = JSON.parse(localStorage.users)
+
     var user = users.find(function (user) {
         return user.email === email || user.username === username
     })
@@ -40,10 +44,35 @@ function registerUser(name, email, username, password) {
     if (user !== undefined) throw new Error('user already exists')
 
     user = {}
+    user.id = uuid()
     user.name = name
     user.email = email
     user.username = username
     user.password = password
 
     users.push(user)
+
+    localStorage.users = JSON.stringify(users)
+}
+
+function getUserName() {
+    var users = JSON.parse(localStorage.user)
+
+    var user = users.find(function (user) {
+        return user.id === sessionStorage.userId
+    })
+
+    if (user === undefined) throw new Error('user not found')
+
+    return user.name
+}
+
+function isUserLoggedIn() {
+    if (sessionStorage.userId !== undefined) return true
+
+    return false
+}
+
+function logoutUser() {
+    delete sessionStorage.userId
 }
