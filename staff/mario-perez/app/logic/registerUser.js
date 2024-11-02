f9517591487146008a98429d0357d8ac5dbbb332
Mariops89 <mariops89@hotmail.com>
2024-11-02 21:08:12 +0100

    add logic, util and view folders(group files), add app file, update app #97

--- staff/mario-perez/app/logic/registerUser.js
+++ staff/mario-perez/app/logic/registerUser.js
@@ -1,21 +1,3 @@
-function loginUser(username, password) {
-    if (typeof username !== 'string') throw new Error('invalid username')
-    if (username.length < 4) throw new Error('invalid username length')
-
-    if (typeof password !== 'string') throw new Error('invalid password')
-    if (password.length < 8) throw new Error('invalid password length')
-
-    var users = JSON.parse(localStorage.users)
-
-    var user = users.find(function (user) {
-        return user.username === username && user.password === password
-    })
-
-    if (user === undefined) throw new Error('wrong credentials')
-
-    sessionStorage.userId = user.id
-}
-
 function registerUser(name, email, username, password) {
     if (typeof name !== 'string') throw new Error('invalid name')
     if (name.length < 1) throw new Error('invalid name length')
@@ -24,8 +6,8 @@ function registerUser(name, email, username, password) {
     if (email.length < 6) throw new Error('invalid email length')
     if (!email.includes('@')) throw new Error('invalid email format')
     if (!email.includes('.')) throw new Error('invalid email format')
-    var indexOfAt = email.indexOf('@')
-    var indexOfDot = email.indexOf('.')
+    const indexOfAt = email.indexOf('@')
+    const indexOfDot = email.indexOf('.')
     if (indexOfDot < indexOfAt) throw new Error('invalid email format')
     // TODO add more rules for email validation (position of @ and .)
 
@@ -35,9 +17,9 @@ function registerUser(name, email, username, password) {
     if (typeof password !== 'string') throw new Error('invalid password')
     if (password.length < 8) throw new Error('invalid password length')
 
-    var users = JSON.parse(localStorage.users)
+    const users = JSON.parse(localStorage.users)
 
-    var user = users.find(function (user) {
+    let user = users.find(function (user) {
         return user.email === email || user.username === username
     })
 
@@ -53,26 +35,4 @@ function registerUser(name, email, username, password) {
     users.push(user)
 
     localStorage.users = JSON.stringify(users)
-}
-
-function getUserName() {
-    var users = JSON.parse(localStorage.users)
-
-    var user = users.find(function (user) {
-        return user.id === sessionStorage.userId
-    })
-
-    if (user === undefined) throw new Error('user not found')
-
-    return user.name
-}
-
-function isUserLoggedIn() {
-    if (sessionStorage.userId !== undefined) return true
-
-    return false
-}
-
-function logoutUser() {
-    delete sessionStorage.userId
 }
