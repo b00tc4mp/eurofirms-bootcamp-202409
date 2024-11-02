de32868129ce2e7f59e0b51af7a30f309d0e9fd3
Mariops89 <mariops89@hotmail.com>
2024-11-02 22:09:37 +0100

    update Arroz.prototype.at #52

--- staff/mario-perez/playground/arroz/Arroz.prototype.at.js
+++ staff/mario-perez/playground/arroz/Arroz.prototype.at.js
@@ -1,33 +1,41 @@
-var Arroz = function () { this.length = 0 }
+class Arroz {
+    constructor() { this.length = 0 }
 
-Arroz.prototype.at = function (position) {
-    //return extracted element in a index position
-    if (position > 0) {
-        return this[position]
-    } else {
-        return this[this.length + position]
+    at(position) {
+        //return extracted element in a index position
+        if (position > 0) {
+            return this[position]
+        } else {
+            return this[this.length + position]
+        }
+
     }
-
 }
+
+
 
 console.log('TEST Arroz.prototype.at')
 
 console.log('CASE return the element in position of parameter function')
 
-var plants = new Arroz
-plants[0] = 'broccoli'
-plants[1] = 'cauliflower'
-plants[2] = 'cabbage'
-plants[3] = 'kale'
-plants[4] = 'tomato'
-plants.length = 5
+{
 
-var index = 2
+    const plants = new Arroz
+    plants[0] = 'broccoli'
+    plants[1] = 'cauliflower'
+    plants[2] = 'cabbage'
+    plants[3] = 'kale'
+    plants[4] = 'tomato'
+    plants.length = 5
 
-var plant = (plants.at(index))
-console.log(plant)
-//cabbage
-index = -2
-plant = (plants.at(index))
-console.log(plant)
-// kale
+    let index = 2
+
+    let plant = (plants.at(index))
+    console.log(plant)
+    //cabbage
+    index = -2
+    plant = (plants.at(index))
+    console.log(plant)
+    // kale
+
+}
