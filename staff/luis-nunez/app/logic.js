41af8724a18953bc78c8b8019b1534e87c96c083
Luis Roberto Nuñez Del Valle <luisdelvalle153@gmail.com>
2024-11-03 10:24:46 +0100

    modified logic.js and main.jsx files #104

--- staff/luis-nunez/app/logic.js
+++ staff/luis-nunez/app/logic.js
@@ -1,16 +1,21 @@
 function loginUser(username, password) {
     if (typeof username !== 'string') throw new Error('invalid username')
     if (username.length < 4) throw new Error('invalid username length')
+
     if (typeof password !== 'string') throw new Error('invalid password')
     if (password.length < 8) throw new Error('invalid password length')
 
-    var users = JSON.parse(localStorage.users)
-    var user = users.find(function (user) {
+    const users = JSON.parse(localStorage.users)
+
+    const user = users.find(function (user) {
         return user.username === username && user.password === password
     })
+
     if (user === undefined) throw new Error('wrong credentials')
+
     sessionStorage.userId = user.id
 }
+
 function registerUser(name, email, username, password) {
     if (typeof name !== 'string') throw new Error('invalid name')
     if (name.length < 1) throw new Error('invalid name length')
@@ -19,18 +24,17 @@ function registerUser(name, email, username, password) {
     if (email.length < 6) throw new Error('invalid email length')
     if (!email.includes('@')) throw new Error('invalid email format')
     if (!email.includes('.')) throw new Error('invalid email format')
-    var indexOfAt = email.indexOf('@')
+    const indexOfAt = email.indexOf('@')
     var indexOfDot = email.indexOf('.')
     if (indexOfDot < indexOfAt) throw new Error('invalid email format')
-    // TODO add more rules for email validation (position of @ and .)
-
+   
     if (typeof username !== 'string') throw new Error('invalid username')
     if (username.length < 4) throw new Error('invalid username length')
 
     if (typeof password !== 'string') throw new Error('invalid password')
     if (password.length < 8) throw new Error('invalid password length')
 
-    var users = JSON.parse(localStorage.users)
+    var users = JSON.parse(localStorage.users || '[]');
 
     var user = users.find(function (user) {
         return user.email === email || user.username === username
