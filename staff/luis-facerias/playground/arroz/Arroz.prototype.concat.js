6ad5d85728e0bab157e977e3215f6e85368a852d
Luis Facerías <luis.facerias@gmail.com>
2024-11-06 23:31:14 +0100

    convert at, concat, fill, find to JavaScript ES6 #53

--- staff/luis-facerias/playground/arroz/Arroz.prototype.concat.js
+++ staff/luis-facerias/playground/arroz/Arroz.prototype.concat.js
@@ -3,61 +3,67 @@
 // Implementation of the Arroz function concat
 // returns one or more merged arrays
 
-var Arroz = function () { this.length = 0 }
-
-Arroz.prototype.concat = function(values) {
-
-    var finalArr = []
-
-    // Insert in array first Arroz
-    for (var i = 0; i < this.length; i++) {
-        finalArr[i] = this[i]
+class Arroz {
+    constructor () { 
+        this.length = 0 
     }
-    // Append in finalArr other array
-    if (arguments.length > 0) {
-        for (var i = 0; i < arguments.length; i++) {
-            var dim_finalArr = finalArr.length
-            for (var j = 0; j < arguments[i].length; j++){
-                finalArr[dim_finalArr + j] = arguments[i][j]
+
+    concat(values) {
+        var finalArr = []
+
+        // Insert in array first Arroz
+        for (var i = 0; i < this.length; i++) {
+            finalArr[i] = this[i]
+        }
+        // Append in finalArr other array
+        if (arguments.length > 0) {
+            for (var i = 0; i < arguments.length; i++) {
+                var dim_finalArr = finalArr.length
+                for (var j = 0; j < arguments[i].length; j++){
+                    finalArr[dim_finalArr + j] = arguments[i][j]
+                }
             }
         }
+        return finalArr
     }
-    return finalArr
 }
 
 // Check if concat merge two or more arrays
 console.log('CASE merge two arrays')
 
-var lowerChar = new Arroz
-lowerChar[0] = 'a'
-lowerChar[1] = 'b'
-lowerChar[2] = 'c'
-lowerChar[3] = 'd'
-lowerChar[4] = 'e'
-lowerChar.length = 5
-//concatlowerChar = lowerChar.concat()
-//console.log(concatlowerChar)
-//[ 'a', 'b', 'c', 'd', 'e' ]
-
-
-var upperChar = new Arroz
-upperChar[0] = 'A'
-upperChar[1] = 'B'
-upperChar[2] = 'C'
-upperChar[3] = 'D'
-upperChar[4] = 'E'
-upperChar.length = 5
-//concatlowerChar = upperChar.concat(lowerChar)
-//console.log(concatlowerChar)
-// [ 'A', 'B', 'C', 'D', 'E', 'a', 'b', 'c', 'd', 'e' ]
-
-var num = new Arroz
-num[0] = '1'
-num[1] = '2'
-num[2] = '3'
-num[3] = '4'
-num[4] = '5'
-num.length = 5
-concatlowerChar = num.concat(lowerChar, upperChar)
-console.log(concatlowerChar)
-// [ '1', '2', '3', '4', '5', 'a', 'b', 'c', 'd', 'e', 'A', 'B', 'C', 'D', 'E' ]
+{
+    var lowerChar = new Arroz
+    lowerChar[0] = 'a'
+    lowerChar[1] = 'b'
+    lowerChar[2] = 'c'
+    lowerChar[3] = 'd'
+    lowerChar[4] = 'e'
+    lowerChar.length = 5
+    concatlowerChar = lowerChar.concat()
+    console.log(concatlowerChar)
+    //[ 'a', 'b', 'c', 'd', 'e' ]
+}
+{
+    var upperChar = new Arroz
+    upperChar[0] = 'A'
+    upperChar[1] = 'B'
+    upperChar[2] = 'C'
+    upperChar[3] = 'D'
+    upperChar[4] = 'E'
+    upperChar.length = 5
+    concatlowerChar = upperChar.concat(lowerChar)
+    console.log(concatlowerChar)
+    // [ 'A', 'B', 'C', 'D', 'E', 'a', 'b', 'c', 'd', 'e' ]
+}
+{
+    var num = new Arroz
+    num[0] = '1'
+    num[1] = '2'
+    num[2] = '3'
+    num[3] = '4'
+    num[4] = '5'
+    num.length = 5
+    concatlowerChar = num.concat(lowerChar, upperChar)
+    console.log(concatlowerChar)
+    // [ '1', '2', '3', '4', '5', 'a', 'b', 'c', 'd', 'e', 'A', 'B', 'C', 'D', 'E' ]
+}
--- staff/luis-facerias/playground/arroz/Arroz.prototype.find.js
+++ staff/luis-facerias/playground/arroz/Arroz.prototype.find.js
@@ -3,42 +3,57 @@
 // Implementation of the Arroz function find
 // returns the first result of a conditional search function
 
-var Arroz = function () { this.length = 0 }
+class Arroz {
+    constructor() {
+        this.length = 0 
+    }
 
-Arroz.prototype.find = function (callback) {
-    for (var i = 0; i < this.length; i++){
-        var element = this[i]
+    find(callback) {
+        for (let i = 0; i < this.length; i++){
+            const element = this[i]
 
-        //var carFound = callback(element)
+            //const carFound = callback(element)
 
-        //if (carFound) return element
+            //if (carFound) return element
 
-        if (callback(element)) return element    // If the function returns true, the loop stops and returns the element
+            if (callback(element)) return element    // If the function returns true, the loop stops and returns the element
+        }
     }
 }
 
 // Check if find returns the first result of a conditional search
 console.log('CASE returns the first result of a conditional search')
+{
+    const cars = new Arroz
+    cars[0] = 'Renault'
+    cars[1] = 'Talbot'
+    cars[2] = 'Ford'
+    cars[3] = 'Seat'
+    cars[4] = 'Skoda'
+    cars[5] = 'Daewo'
+    cars[6] = 'Toyota'
+    cars.length = 7
+    const car = cars.find(function (car) {
+        return car.startsWith('S')
+    })
+    console.log(car)
+    // Seat
+}
 
-var cars = new Arroz
-cars[0] = 'Renault'
-cars[1] = 'Talbot'
-cars[2] = 'Ford'
-cars[3] = 'Seat'
-cars[4] = 'Skoda'
-cars[5] = 'Daewo'
-cars[6] = 'Toyota'
-cars.length = 7
-var car = cars.find(function (car) {
-    return car.startsWith('S')
-})
-console.log(car)
-// Seat
-
-
-console.log('CASE returns the first result of car with k its name')
-var car = cars.find(function (car) {
-    return car.includes('k')
-})
-console.log(car)
-// Skoda
+{
+    console.log('CASE returns the first result of car with k its name')
+    const cars = new Arroz
+    cars[0] = 'Renault'
+    cars[1] = 'Talbot'
+    cars[2] = 'Ford'
+    cars[3] = 'Seat'
+    cars[4] = 'Skoda'
+    cars[5] = 'Daewo'
+    cars[6] = 'Toyota'
+    cars.length = 7
+    const car = cars.find(function (car) {
+        return car.includes('k')
+    })
+    console.log(car)
+    // Skoda
+}
