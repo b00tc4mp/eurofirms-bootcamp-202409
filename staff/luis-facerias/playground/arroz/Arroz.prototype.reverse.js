560abe551f1d142581b03fce8dfdbfd0b52a2433
Luis Facerías <luis.facerias@gmail.com>
2024-11-06 21:05:49 +0100

    add final arroz reverse code #53

--- staff/luis-facerias/playground/arroz/Arroz.prototype.reverse.js
+++ staff/luis-facerias/playground/arroz/Arroz.prototype.reverse.js
@@ -8,9 +8,35 @@ var Arroz = function() { this.length = 0 }
 
 Arroz.prototype.reverse = function () {
     reversed = []
-    for (var i = this.length - 1; i = 0; i--){
-        for (var j = 0; j < this.length; i++){
-            reversed[j] = this[i]
-        }
+    for (var i = 0; i < this.length; i++){
+        this[i].length === 0 ? this[i] = '<1 empty item>': this[i]
+        reversed[this.length - 1 - i] = this[i]
     }
+    return reversed
 }
+
+console.log('TEST Arroz.prototype.reverse')
+console.log('CASE returns an array with elements in reverse order')
+
+var cars = new Arroz
+cars[0] = 'Renault'
+cars[1] = 'Tata'
+cars[2] = 'Simca'
+cars[3] = 'Talbot'
+cars[4] = 'Tesla'
+cars.length = 5
+var element = cars.reverse()
+console.log(element)
+
+
+console.log('CASE returns an reversed array with some empty elements')
+
+var cars = new Arroz
+cars[0] = 'Renault'
+cars[1] = 'Tata'
+cars[2] = 'Simca'
+cars[3] = ''
+cars[4] = 'Tesla'
+cars.length = 5
+var element = cars.reverse()
+console.log(element)
