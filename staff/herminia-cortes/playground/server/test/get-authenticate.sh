e2d697c54269f2a70e189c01dbe4d6406bfec049
hcg197513 <herminiacortesgomez1975@gmail.com>
2024-11-07 20:30:28 +0100

    add get-authenticate.sh, get-default.sh, get-helloworld.sh, get-salutation.sh, get-time.sh, post-authenticate.sh, index.js, package-lock.json, package.json to test #61

---
+++ staff/herminia-cortes/playground/server/test/get-authenticate.sh
@@ -0,0 +1,1 @@
+curl -X GET 'http://localhost:8080/authenticate?username=mikemouse&password=123123123' -v
---
+++ staff/herminia-cortes/playground/server/test/post-authenticate.sh
@@ -0,0 +1,1 @@
+curl -X POST 'http://localhost:8080/authenticate?username=mikeymouse&password=123123123' -v
