b7b7e022dbdd835c8020570c847147286b7cf17b
Manuel Barzi <manuelbarzi@gmail.com>
2024-11-08 21:34:28 +0100

    add authenticate-user and get-user-name logics and integrate them in api #94

--- staff/manuel-barzi/product/api/index.js
+++ staff/manuel-barzi/product/api/index.js
@@ -1,6 +1,8 @@
 import express, { json } from 'express'
 
 import registerUser from './logic/registerUser.js'
+import authenticateUser from './logic/authenticateUser.js'
+import getUserName from './logic/getUserName.js'
 
 const api = express()
 
@@ -18,6 +20,34 @@ api.post('/users', jsonBodyParser, (req, res) => {
         registerUser(name, email, username, password)
 
         res.status(201).send()
+    } catch (error) {
+        res.status(400).json({ error: error.constructor.name, message: error.message })
+    }
+})
+
+api.post('/users/auth', jsonBodyParser, (req, res) => {
+    try {
+        const username = req.body.username
+        const password = req.body.password
+
+        const userId = authenticateUser(username, password)
+
+        res.json(userId)
+    } catch (error) {
+        res.status(400).json({ error: error.constructor.name, message: error.message })
+    }
+})
+
+api.get('/users/:targetUserId/name', (req, res) => {
+    try {
+        const authorization = req.headers.authorization // Basic <user-id>
+        const userId = authorization.slice(6)
+
+        const targetUserId = req.params.targetUserId
+
+        const name = getUserName(userId, targetUserId)
+
+        res.json(name)
     } catch (error) {
         res.status(400).json({ error: error.constructor.name, message: error.message })
     }
