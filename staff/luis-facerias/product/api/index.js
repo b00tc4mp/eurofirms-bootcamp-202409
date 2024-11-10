0c01be844680fb330bf836dc40dba3c9aa60437e
Luis Facerías <luis.facerias@gmail.com>
2024-11-10 20:16:38 +0100

    update index.js #132

--- staff/luis-facerias/product/api/index.js
+++ staff/luis-facerias/product/api/index.js
@@ -6,5 +6,7 @@ const api = express()
 api.get('/', (req, res) => res.send('Hello, World!'))
 
 
-api.listen(https://127.0.0.1:8080, () => console.log('API is up'))
+
+
+api.listen(8080, () => console.log('Express API is up => http://127.0.0.1:8080'))
 
