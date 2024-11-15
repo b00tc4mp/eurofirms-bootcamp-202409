0b2d0d0f75d92bf4b668f1213c5f3987ef767616
Rosana Perez <rosanaperezrios@gmail.com>
2024-11-15 18:36:38 +0100

    add reduce test in array #64

--- staff/rosana-perez/playground/array/Array.prototype.reduce.js
+++ staff/rosana-perez/playground/array/Array.prototype.reduce.js
@@ -13,6 +13,8 @@ const reducedNumbers =  numbers.reduce(function (accumulator, currentValue) {
 
 console.log(numbers)
 console.log(reducedNumbers);
+
+// también puede configurarse como: var sum = numbers.reduce(function(accum, num){return accum + num}, 0)
 }
 
 {console.log('CASO 2.- método reduce sin valor inicial')
@@ -42,3 +44,37 @@ const reducedLetras = letras.reduce((accumulator, currentValue) => accumulator +
 
 console.log(letras);
 console.log(reducedLetras);
+
+
+console.log('CASO 5 -> Extract total from cart')
+
+var cart = [
+    {brand: 'colgate', title: 'tooth paste', type: 'hygiene', quantity: 2, price: 3},
+    {brand: 'aldi', title: 'avena milk', type: 'food', quantity: 3, price: 1.2},
+    {brand: 'aldi', title: 'humus', type: 'food',quantity: 1, price: 2.5},
+    {brand: 'aldi', title: 'beans', type: 'food',quantity: 4, price: 2.1}
+]
+
+var total = cart.reduce (function (total, item) {
+    return total + item.quantity * item.price
+}, 0)
+
+console.log(total)
+//expected output -> 20.5
+
+
+console.log('CASO 6 -> extract total count hygiene and food products from cart')
+
+var stats = cart.reduce(function(stats, item) {
+    if (item.type === 'hygiene') //si el item.type es 'hygiene', 
+        stats.hygiene++ // sumar en stats.hygiene
+    else if(item.type === 'food')
+        stats.food++
+
+    return stats
+}, {hygiene: 0, food: 0}) // poner contadores a cero, en un objeto con los dos item.type
+
+console.log(stats)
+//expected output-> hygiene: 1, food: 3
+
+
