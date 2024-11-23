17c1572ab17859aa8c1bff784009b85e0bb1cf9a
Manuel Barzi <manuelbarzi@gmail.com>
2024-11-23 17:54:57 +0100

    add mongoose to all api logics #94

--- staff/manuel-barzi/product/api/logic/deletePost.test.js
+++ staff/manuel-barzi/product/api/logic/deletePost.test.js
@@ -1,7 +1,16 @@
+import mongoose from 'mongoose'
 import deletePost from './deletePost.js'
 
-try {
-    deletePost('4qgqeh5z01k', '123abc456def')
-} catch (error) {
-    console.error(error)
-}
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+    .then(() => {
+        try {
+            return deletePost('6741f97a50976d72be46fe89', '6742012bc981b1956d309476')
+                .then(() => console.log('post deleted'))
+                .catch(error => console.error(error))
+        } catch (error) {
+            console.error(error)
+        }
+    })
+    .then(() => console.log('the end'))
+    .catch(error => console.error(error))
+    .finally(() => mongoose.disconnect())
