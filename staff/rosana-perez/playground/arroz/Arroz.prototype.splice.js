b6251d4152fda7dfa093f39195b18a844206ad6d
Rosana Perez <rosanaperezrios@gmail.com>
2024-11-25 22:03:41 +0100

    add new cases of prototype splice in arroz and copy file #64

--- staff/rosana-perez/playground/arroz/Arroz.prototype.splice.js
+++ staff/rosana-perez/playground/arroz/Arroz.prototype.splice.js
@@ -11,47 +11,102 @@ class Arroz {
     }
 
 
-    splice(start, deleteCount, item) {
-        // si existe un valor en deleteCount, borrar los elementos que corresponden
-        // si existe item, añadirlo en la posición que corresponda
-        //  
-        if (deleteCount === 0 && item !== undefined) {
+    splice(start, deleteAmount, elementsToAdd) {  // names.splice(1,2) start ->1, deleteAmount ->2, length: 8, elementsToAdd-> undefined
+        //crear deletedElements = [] -> aqui guardo los elemtos borrados
+        let deletedElements = []
 
-            for (let i = this.length - 1; i >= start; i--) {
-                this[i + 1] = this[i]
 
-            }
-            this.length++
-            this[start] = item
+        //PASO 1 -> añadir a deletedElements los elementos a borrar despues
 
-            return this
+        //{0: 'Peter', 1: 'John', 2: 'Max', 3: 'James', 4: 'Constantin',  5: 'Mario', 6: 'Tea', 7: 'Luis', length: 8}
+        //deletedElements  = []
+
+        for (let i = start; i < start + deleteAmount; i++) {
+            deletedElements.push(this[i])
+            //{0: 'Peter', 1: 'John', 2: 'Max', 3: 'James', 4: 'Constantin',  5: 'Mario', 6: 'Tea', 7: 'Luis', length: 8}
+            //deletedElements  = ['John']
+
+            //{0: 'Peter', 1: 'John', 2: 'Max', 3: 'James', 4: 'Constantin',  5: 'Mario', 6: 'Tea', 7: 'Luis', length: 8}
+            //deletedElements  = ['John', 'Max']
+        }
+
+        //PASO 2 -> borrar la cantidad de elementos pedida (deletedAmount) desde la posicion indicada (start)
+        // names.splice(1,2) start ->1, deleteAmount ->2, length: 8, elementsToAdd-> undefined
+
+        //{0: 'Peter', 1: 'John', 2: 'Max', 3: 'James', 4: 'Constantin',  5: 'Mario', 6: 'Tea', 7: 'Luis', length: 8}
+
+        for (let i = start; i < start + deleteAmount; i++) {
+            delete this[i]
+            this.length--
+        }
+        //i = 1, i<3, this[1]= John; delete John; length 7
+        //i = 2, i<3,  this[2]= Max; delete Max; length 6
+        //i = 3; i = 3 -> stop bucle
+
+        //{0: 'Peter', 2: 'Max', 3: 'James', 4: 'Constantin',  5: 'Mario', 6: 'Tea', 7: 'Luis', length: 7}
+        //{0: 'Peter', 3: 'James', 4: 'Constantin',  5: 'Mario', 6: 'Tea', 7: 'Luis', length: 6}
+
+
+        //PASO 3 -> mover los elementos posteriores a start cada una de posiciones igual a deletedAmount
+
+        //{0: 'Peter', 3: 'James', 4: 'Constantin',  5: 'Mario', 6: 'Tea', 7: 'Luis', length: 6}
+
+        for (let i = start; i < this.length; i++) {
+            this[i] = this[i + deleteAmount]// this[1] = this[1+2 = 3]
+            // this[2] = this[2+2= 4]
+            // this[3] = this[3+2= 5]
+            //this[4] = this[4+2=6]
+            //this[5] = this[5+2=7]
+            //*** stop
+
 
         }
-        console.log(this)
-        /* const objeto = { a: 1, b: 2 };
-        objeto.c = 3; // Agrega un nuevo elemento con clave "c" y valor 3
-        console.log(objeto); // { a: 1, b: 2, c: 3 } */
+
+        for (let i = this.length + start; i >= this.length; i--) {
+
+            delete this[i]
+        }
+
+        //delete this[]
+
+        // names.splice(1,2) start ->1, deleteAmount ->2, length: 6
+        //this[start] = this[start+deleteAmount] -> 
+        //{0: 'Peter', 1: 'James', 4: 'Constantin',  5: 'Mario', 6: 'Tea', 7: 'Luis', length: 6}
+
+        //this[start+1] = this[start+deletedAmount+1] -> this[2] = this[3+1]
+
+
+        //{0: 'Peter', 1: 'James', 2: 'Constantin',  5: 'Mario', 6: 'Tea', 7: 'Luis', length: 6}
+
+        //{0: 'Peter', 1: 'James', 2: 'Constantin',  3: 'Mario', 6: 'Tea', 7: 'Luis', length: 6}
+
+        //{0: 'Peter', 1: 'James', 2: 'Constantin',  3: 'Mario', 4: 'Tea', 7: 'Luis', length: 6}
+
+        //{0: 'Peter', 1: 'James', 2: 'Constantin',  3: 'Mario', 4: 'Tea', 5: 'Luis', length: 6}
+
+
+        return deletedElements
     }
 
 }
 
 
-const movies = new Arroz();
 
-movies[0] = 'karate kid'
-movies[1] = 'vacaciones en roma'
-movies[2] = 'seven'
-movies[3] = 'men in black'
-movies[4] = 'forrest gump'
-movies[5] = 'el guardaespaldas'
-movies[6] = 'interstellar'
-movies[7] = 'WIP'
-movies.length = 8
+const names = new Arroz();
 
-movies.splice(2, 0, 'IA Robot')
+names[0] = 'Peter'
+names[1] = 'John'
+names[2] = 'Max'
+names[3] = 'James'
+names[4] = 'Constantin'
+names[5] = 'Mario'
+names[6] = 'Tea'
+names[7] = 'Luis'
+names.length = 8
 
-// ir a la posición 2 -> start <-
-// borrar un elemento -> delete this[start] <-
-// añadir un item -> this.start = item 
+console.log('caso 1 -> desde la posicion 1, borrar 2 elementos y no insertar nada')
+console.log('elementos eliminados del caso 1 ', names.splice(1, 2))
+//[John, MaX]
 
-console.log('CASO 1', movies)
+console.log('names después de caso 1-> ', names)
+//{0: 'Peter', 1: 'James', 2: 'Constantin', 3: 'Mario', 4: 'Tea', 5: 'Luis', length: 6}
