674fdf88b922848eb931422bea58c222204d70f0
ousman <oman7910@gmail.com>
2024-12-04 18:12:00 +0100

    cambio en el archivo css, postcss, tailwin #103

---
+++ staff/ousman-krubally/product/app/tailwind.config.js
@@ -0,0 +1,12 @@
+/**@type {import('tailwindcss').config} */
+export default {
+    content: [
+        './index.html',
+        './app.jsx',
+        './view/*.jsx'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
