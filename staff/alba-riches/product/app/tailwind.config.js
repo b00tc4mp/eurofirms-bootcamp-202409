99231721d6c205182121050c62de26713e8e0416
Alba Riches <alba.ches@gmail.com>
2024-12-07 13:40:28 +0100

    add dotenv#117

--- staff/alba-riches/product/app/tailwind.config.js
+++ staff/alba-riches/product/app/tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [ // le decimos a tailwind que ficheros se tiene que mirar para luego poner estilos con css
-        './index.html',
-        './App.jsx',
-        './view/*.jsx'
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+  content: [
+    './index.html',
+    './App.jsx',
+    './view/*.jsx'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
+
