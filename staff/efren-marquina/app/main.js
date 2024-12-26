d8e887bfdd69bb340f90fa6698725a7166016ab9
Efren-25 <efrenmarquina@gmail.com>
2024-12-26 06:07:43 +0100

    implement all app views (welcome, login, register,home) #148

--- staff/efren-marquina/app/main.js
+++ staff/efren-marquina/app/main.js
@@ -1,130 +1,310 @@
+var body = document.querySelector('body')
 
-//presentation
+var title = document.createElement('h1')
+title.innerText = 'App'
+body.appendChild(title)
 
-var sections = document.querySelectorAll('section')
+// welcome view
 
-var welcomeSection = sections[0]
-var registerSection = sections[1]
-var loginSection = sections[2]
-var homeSection = sections[3]
+/*
+html
+    head
+    body
+        main
+            h2
+                text
+            p
+                text
+                a
+                text
+                a
+                text
+*/
 
-registerSection.style.display = 'none'
-loginSection.style.display = 'none'
-homeSection.style.display = 'none'
+var welcomeView = document.createElement('main')
+body.appendChild(welcomeView)
 
-var welcomeLinks = welcomeSection.querySelectorAll('a')
+var welcomeTitle = document.createElement('h2')
+welcomeTitle.innerText = 'Welcome!'
+welcomeView.appendChild(welcomeTitle)
 
-var welcomeRegisterLink = welcomeLinks[0]
-var welcomeLoginLink = welcomeLinks[1]
+var welcomeIntro = document.createElement('p')
+welcomeView.appendChild(welcomeIntro)
+
+var welcomeIntroPart1 = new Text('Please, ')
+welcomeIntro.appendChild(welcomeIntroPart1)
+
+var welcomeRegisterLink = document.createElement('a')
+welcomeRegisterLink.href = ''
+welcomeRegisterLink.innerText = 'Register'
+welcomeIntro.appendChild(welcomeRegisterLink)
 
 welcomeRegisterLink.addEventListener('click', function (event) {
     event.preventDefault()
 
-    welcomeSection.style.display = 'none'
-    registerSection.style.display = ''            
+    body.removeChild(welcomeView)
+    body.appendChild(registerView)
 })
+
+var welcomeIntroPart3 = new Text(' or ')
+welcomeIntro.appendChild(welcomeIntroPart3)
+
+var welcomeLoginLink = document.createElement('a')
+welcomeLoginLink.href = ''
+welcomeLoginLink.innerText = 'Login'
+welcomeIntro.appendChild(welcomeLoginLink)
 
 welcomeLoginLink.addEventListener('click', function (event) {
     event.preventDefault()
 
-    welcomeSection.style.display = 'none'
-    loginSection.style.display =''
+    body.removeChild(welcomeView)
+    body.appendChild(loginView)
 })
 
-var registerLinks = registerSection.querySelectorAll('a')
+var welcomeIntroPart5 = new Text('.')
+welcomeIntro.appendChild(welcomeIntroPart5)
 
-var registerLoginLink = registerLinks[0]
+// register view
 
-registerLoginLink.addEventListener('click', function (event) {
-    event.preventDefault()
+/*
+html
+    body
+        main
+            h2
+                text
+            form
+                label
+                    text
+                input
+                label
+                    text
+                input
+                label
+                    text
+                input
+                label
+                    text
+                input
+            p
+            a
 
-    registerSection.style.display = 'none'
-    loginSection.style.display = ''
-})
+*/
 
-var loginLinks =loginSection.querySelectorAll('a')
+var registerView = document.createElement('main')
 
-var loginRegisterLink = loginLinks [0]
+var registerTitle = document.createElement('h2')
+registerTitle.innerText = 'Register'
+registerView.appendChild(registerTitle)
 
-loginRegisterLink.addEventListener('click', function (event) {
-    event.preventDefault()
-
-    loginSection.style.display = 'none'
-    registerSection.style.display = ''
-})
-
-var registerForm = registerSection.querySelector('form')
+var registerForm = document.createElement('form')
+registerView.appendChild(registerForm)
 
 registerForm.addEventListener('submit', function (event) {
     event.preventDefault()
-
-    var registerFormInputs = registerForm.querySelectorAll('input')
-
-    var registerFormNameInput = registerFormInputs[0]
-    var registerFormEmailInput = registerFormInputs[1]
-    var registerFormUsernameInput = registerFormInputs[2]
-    var registerFormPasswordInput = registerFormInputs[3]
 
     var name = registerFormNameInput.value
     var email = registerFormEmailInput.value
     var username = registerFormUsernameInput.value
     var password = registerFormPasswordInput.value
 
-    var feedback = registerSection.querySelector('p')
-
-    try{
+    try {
         registerUser(name, email, username, password)
-   
+
         registerForm.reset()
-        feedback.innerText = ''
-    
-        registerSection.style.display = 'none'
-        loginSection.style.display = ''
-    } catch(error) {
-        feedback.innerText = error.message
+        registerFeedback.innerText = ''
+
+        body.removeChild(registerView)
+        body.appendChild(loginView)
+    } catch (error) {
+        registerFeedback.innerText = error.message
 
         console.error(error)
     }
 })
 
-var loginForm = loginSection.querySelector('form')
+var registerFormNameLabel = document.createElement('label')
+registerFormNameLabel.htmlFor = 'name'
+registerFormNameLabel.innerText = 'Name'
+registerForm.appendChild(registerFormNameLabel)
 
-loginForm.addEventListener('submit', function(event) {
+var registerFormNameInput = document.createElement('input')
+registerFormNameInput.type = 'text'
+registerFormNameInput.id = 'name'
+registerForm.appendChild(registerFormNameInput)
+
+var registerFormEmailLabel = document.createElement('label')
+registerFormEmailLabel.htmlFor = 'email'
+registerFormEmailLabel.innerText = 'E-mail'
+registerForm.appendChild(registerFormEmailLabel)
+
+var registerFormEmailInput = document.createElement('input')
+registerFormEmailInput.type = 'email'
+registerFormEmailInput.id = 'email'
+registerForm.appendChild(registerFormEmailInput)
+
+var registerFormUsernameLabel = document.createElement('label')
+registerFormUsernameLabel.htmlFor = 'username'
+registerFormUsernameLabel.innerText = 'Username'
+registerForm.appendChild(registerFormUsernameLabel)
+
+var registerFormUsernameInput = document.createElement('input')
+registerFormUsernameInput.type = 'text'
+registerFormUsernameInput.id = 'username'
+registerForm.appendChild(registerFormUsernameInput)
+
+var registerFormPasswordLabel = document.createElement('label')
+registerFormPasswordLabel.htmlFor = 'password'
+registerFormPasswordLabel.innerText = 'Password'
+registerForm.appendChild(registerFormPasswordLabel)
+
+var registerFormPasswordInput = document.createElement('input')
+registerFormPasswordInput.type = 'password'
+registerFormPasswordInput.id = 'password'
+registerForm.appendChild(registerFormPasswordInput)
+
+var registerFormSubmitButton = document.createElement('button')
+registerFormSubmitButton.type = 'submit'
+registerFormSubmitButton.innerText = 'Register'
+registerForm.appendChild(registerFormSubmitButton)
+
+var registerFeedback = document.createElement('p')
+registerView.appendChild(registerFeedback)
+
+var registerLoginLink = document.createElement('a')
+registerLoginLink.href = ''
+registerLoginLink.innerText = 'Login'
+registerView.appendChild(registerLoginLink)
+
+registerLoginLink.addEventListener('click', function (event) {
     event.preventDefault()
 
-    var loginFormInputs = loginForm.querySelectorAll('input')
+    body.removeChild(registerView)
+    body.appendChild(loginView)
+})
 
-    var loginFromUsernameInput = loginFormInputs[0]
-    var loginFormPasswordInput = loginFormInputs[1]
+// login view
 
-    var username = loginFromUsernameInput.value
+/*
+html
+    body
+        main
+            h2
+                text
+            form
+                label
+                    text
+                input
+                label
+                    text
+                input
+            p
+            a
+
+*/
+
+var loginView = document.createElement('main')
+
+var loginTitle = document.createElement('h2')
+loginTitle.innerText = 'Login'
+loginView.appendChild(loginTitle)
+
+var loginForm = document.createElement('form')
+loginView.appendChild(loginForm)
+
+loginForm.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    var username = loginFormUsernameInput.value
     var password = loginFormPasswordInput.value
 
-    var feedback = loginSection.querySelector('p')
-
-    try{
+    try {
         var user = authenticateUser(username, password)
 
         loginForm.reset()
-        feedback.innerText = ''
-        
-        loginSection.style.display = 'none'
-        homeSection.style.display = ''
+        loginFeedback.innerText = ''
 
-        var userTitle = homeSection.querySelector('h3')
-        userTitle.innerText = 'Hello, ' + user.name + '!'
-    } catch(error) {
-        feedback.innerText = error.message
-    
+        body.removeChild(loginView)
+        body.appendChild(homeView)
+
+        homeUser.innerText = 'Hello, ' + user.name + '!'
+    } catch (error) {
+        loginFeedback.innerText = error.message
+
         console.error(error)
     }
 })
 
-var logoutButton = homeSection.querySelector('button')
+var loginFormUsernameLabel = document.createElement('label')
+loginFormUsernameLabel.htmlFor = 'username'
+loginFormUsernameLabel.innerText = 'Username'
+loginForm.appendChild(loginFormUsernameLabel)
 
-logoutButton.addEventListener('click', function(event) {
+var loginFormUsernameInput = document.createElement('input')
+loginFormUsernameInput.type = 'text'
+loginFormUsernameInput.id = 'username'
+loginForm.appendChild(loginFormUsernameInput)
+
+var loginFormPasswordLabel = document.createElement('label')
+loginFormPasswordLabel.htmlFor = 'password'
+loginFormPasswordLabel.innerText = 'Password'
+loginForm.appendChild(loginFormPasswordLabel)
+
+var loginFormPasswordInput = document.createElement('input')
+loginFormPasswordInput.type = 'password'
+loginFormPasswordInput.id = 'password'
+loginForm.appendChild(loginFormPasswordInput)
+
+var loginFormSubmitButton = document.createElement('button')
+loginFormSubmitButton.type = 'submit'
+loginFormSubmitButton.innerText = 'Login'
+loginForm.appendChild(loginFormSubmitButton)
+
+var loginFeedback = document.createElement('p')
+loginView.appendChild(loginFeedback)
+
+var loginRegisterLink = document.createElement('a')
+loginRegisterLink.href = ''
+loginRegisterLink.innerText = 'Register'
+loginView.appendChild(loginRegisterLink)
+
+loginRegisterLink.addEventListener('click', function (event) {
     event.preventDefault()
 
-    homeSection.style.display = 'none'
-    loginSection.style.display = ''
+    body.removeChild(loginView)
+    body.appendChild(registerView)
+})
+
+// home view
+
+/*
+html
+    body
+        main
+            h2
+                text
+            h3
+                text
+            button
+                text
+*/
+
+var homeView = document.createElement('main')
+
+var homeTitle = document.createElement('h2')
+homeTitle.innerText = 'Home'
+homeView.appendChild(homeTitle)
+
+var homeUser = document.createElement('h3')
+homeUser.innerText = 'Hello, User!'
+homeView.appendChild(homeUser)
+
+var homeLogoutButton = document.createElement('button')
+homeLogoutButton.innerText = 'Logout'
+homeView.appendChild(homeLogoutButton)
+
+homeLogoutButton.addEventListener('click', function (event) {
+    event.preventDefault()
+
+    body.removeChild(homeView)
+    body.appendChild(loginView)
 })
