6c53957cf430b9648a4d23931b5fefdbfef0f98a
Luis Facerías <luis.facerias@gmail.com>
2024-12-28 14:57:31 +0100

    separate presentation logic from rendering in handle functions #132

--- staff/luis-facerias/product/app/view/Register.jsx
+++ staff/luis-facerias/product/app/view/Register.jsx
@@ -7,61 +7,65 @@ import registerUser from '../logic/registerUser'
 function Register(props) {
     console.log('Register -> render')
 
-    return <main>
-        <h2>Register</h2>
+    const handleRegisterSubmit = event => {
+        event.preventDefault()
 
-        <form onSubmit={event => {
-            event.preventDefault()
+        const form = event.target
 
-            const form = event.target
+        const name = form.name.value
+        const email = form.email.value
+        const username = form.username.value
+        const password = form.password.value
 
-            const name = form.name.value
-            const email = form.email.value
-            const username = form.username.value
-            const password = form.password.value
+        try {
+            registerUser(name, email, username, password)
+                .then(() => props.onRegisterSuccess())
+                .catch(error => {
+                    if (error instanceof DuplicityError)
+                        alert(error.message)
+                    else if (error instanceof SystemError)
+                        alert('sorry, there was a problem. try again later.')
 
-            try {
-                registerUser(name, email, username, password)
-                    .then(() => props.onRegisterSuccess())
-                    .catch(error => {
-                        if (error instanceof DuplicityError)
-                            alert(error.message)
-                        else if (error instanceof SystemError)
-                            alert('sorry, there was a problem. try again later.')
+                    console.error(error)
+                })
+        } catch (error) {
+            if (error instanceof ValidationError)
+                alert(error.message)
+            else
+                alert('sorry, there was a problem. try again later.')
 
-                        console.error(error)
-                    })
-            } catch (error) {
-                if (error instanceof ValidationError)
-                    alert(error.message)
-                else
-                    alert('sorry, there was a problem. try again later.')
+            console.error(error)
+        }
+    }
 
-                console.error(error)
-            }
-        }}>
+    const handleLoginClick = event => {
+        event.preventDefault()
+
+        props.onLoginClick()
+    }
+
+    return <main className="p-20">
+        <h2 className="text-3xl">Register</h2>
+
+        <form className="flex flex-col gap-2" onSubmit={handleRegisterSubmit}>
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" />
+            <input className="border-2 border-black px-2" type="text" id="name" />
 
             <label htmlFor="email">E-mail</label>
-            <input type="email" id="email" />
+            <input className="border-2 border-black px-2" type="email" id="email" />
 
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" />
+            <input className="border-2 border-black px-2" type="text" id="username" />
 
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
+            <input className="border-2 border-black px-2" type="password" id="password" />
 
-            <button type="submit">Register</button>
+            <button className="bg-black text-white" type="submit">Register</button>
         </form>
 
         <p></p>
 
-        <a href="" onClick={event => {
-            event.preventDefault()
-
-            props.onLoginClick()
-        }}>Login</a>
+        <a className="underline" href="" onClick={handleLoginClick}>Login</a>
     </main>
 }
 
