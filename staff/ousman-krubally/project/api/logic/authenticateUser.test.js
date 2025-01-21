c4ff6effd7c6421ce51506d560abb439296ab55e
ousman <oman7910@gmail.com>
2025-01-21 09:53:51 +0100

    add authenticateUser.js and a.tes #146

--- staff/ousman-krubally/project/api/logic/authenticateUser.test.js
+++ staff/ousman-krubally/project/api/logic/authenticateUser.test.js
@@ -1,4 +1,16 @@
-import mongoose from 
-import authenticateUser from
+import mongoose from 'mongoose'
+import authenticateUser from './authenticateUser.js'
 
-mongoose.connect
+mongoose.connect('mongodb://127.0.0.1:27017/mattas')
+.then(() => {
+    try {
+        return authenticateUser('piterpan', '123123123')
+            .then(userId => console.log(userId))
+            .catch(error => console.error(error))
+    } catch (error) {
+        console.error(error)
+    }
+})
+.then(() => console.log('then end'))
+.catch(error => console.error(error))
+.finally(() => mongoose.disconnect())
